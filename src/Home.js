a4e53793de27682e2eb3520dc86fee519a5295ef
Josef Schönbäck <69212054+strobosaur@users.noreply.github.com>
2021-09-24 17:04:00 +0200

    useFetch custom hook

--- src/Home.js
+++ src/Home.js
@@ -1,48 +1,20 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-  // const [blogs, setBlogs] = useState([
-  //   { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-  //   { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-  //   { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'luigi', id: 3 }
-  // ]);  
-  
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
+  const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  }
-
-  useEffect(() => {
-    fetch('http://localhost:8000/blogs')
-      .then((result) => {
-        if(!result.ok) {
-          throw Error('No data in response');
-        }
-        return result.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setBlogs(data);
-        setIsPending(false);
-        setError(null);
-      })
-      .catch((error) => {
-        console.log(error.message);
-        setIsPending(false);
-        setError(error.message);
-      })
-  }, []);
+  // const handleDelete = (id) => {
+  //   const newBlogs = blogs.filter(blog => blog.id !== id);
+  //   setBlogs(newBlogs);
+  // }
 
   return (
     <div className="home">
       { error && <div>{ error }</div> }
       { isPending && <div>Loading...</div> }
-      { blogs && <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />}
+      { blogs && <BlogList blogs={blogs} title="All blogs" />}
     </div>
    );
 }
